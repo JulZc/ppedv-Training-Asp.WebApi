571e4e2bb0d2f2885133211b6ce29c62bbf79a98
jz1 <jz1@WIN10-JZ1>
2017-05-31 15:19:54 +0200

    Adding same project as selfhost

---
+++ WebApi Selfhost Course ppedv/Controllers/TrainingsController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using Trainings.Core;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
+
+namespace WebApi_Course_ppedv.Controllers
+{
+
+    public class TrainingsController : ApiController
+    {
+        private readonly TrainingsContext db;
+
+        public TrainingsController()
+        {
+            db = new TrainingsContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+
+
+
+
+        //GET api/Trainings
+        public async Task<IHttpActionResult> Get()
+        {
+            List<Training> trainings = await db.Trainings.Include(x => x.Category).ToListAsync();
+            return Ok(trainings);
+        }
+
+        //GET api/Trainings/3
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            Training training = await db.Trainings.Include(x => x.Category).SingleOrDefaultAsync(t => t.Id == id);
+
+            if (training == null)
+                return NotFound();
+            else
+                return Ok(training);
+        }
+
+        [Route("api/category/{id:int}/trainings")]
+        public async Task<IHttpActionResult> GetByGenre(int id)
+        {
+            List<Training> trainings = await db.Trainings.Include(t => t.Category).Where(t => t.CategoryId == id).ToListAsync();
+
+            if (!trainings.Any())
+                return BadRequest();
+
+            return Ok(trainings);
+
+        }
+
+        [Route("api/category/{name:alpha}/trainings")]
+        public async Task<IHttpActionResult> GetByGenre(string name)
+        {
+            List<Training> trainings = await db.Trainings.Include(t => t.Category).Where(t => t.Category.Name == name).ToListAsync();
+
+            if (!trainings.Any())
+                return BadRequest();
+
+            return Ok(trainings);
+
+        }
+
+
+        //POST api/Trainings
+        public async Task<IHttpActionResult> Post([FromBody]Training training)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            db.Trainings.Add(training);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = training.Id }, training);
+        }
+
+        //PUT api/Trainings/3
+        public async Task<IHttpActionResult> Put(int id, [FromBody] Training training)
+        {
+            if (id != training.Id || !ModelState.IsValid)
+                return BadRequest(ModelState);
+
+
+            db.Entry(training).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+
+            return Ok();
+        }
+
+
+        //DELETE api/Trainings/3
+        [Authorize(Roles = "Admin")]
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Delete(int id)
+        {
+            var training = db.Trainings.Find(id);
+
+            if (training == null)
+                return NotFound();
+
+
+            db.Entry(training).State = EntityState.Deleted;
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
